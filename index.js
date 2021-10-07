cd0a62a94a8e47e4a5e4c7ee438e49ac7a0c99ef
Kristofer <skipperk@interia.pl>
2021-10-07 19:34:30 +0200

    2.1

---
+++ index.js
@@ -0,0 +1,12 @@
+// "use strict";
+// message = "ssss";
+// var message = 'aaaaa';
+//
+//console.log(message)
+
+//cost - stała zmienna, musio zostać zadeklarowana wartość, nie da się jej zmienić
+//let stworzenie zmiennej, której wartośc może być nadana później lub może zostać zmieniona
+const number = 11;
+let age;
+age = 22;
+console.log(age);
