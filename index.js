ebe9e3beb0c352910e1161bd934a18d5e85f6ba0
Kristofer <skipperk@interia.pl>
2021-10-09 21:47:15 +0200

    3.3

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const czyPotwierdzony = false;
-// let iloscPrzedmiotow = 0;
-/*
-if (czyPotwierdzony) {
-    iloscPrzedmiotow = 1;
-} else {
-    alert("Proszę się zalogować!")
-    iloscPrzedmiotow = 0;
-}
-*/
-const iloscPrzedmiotow = czyPotwierdzony ? 1 : console.log("Proszę się zalogować!");
-console.log(czyPotwierdzony);
+const odp = "imię";
+const czyZweryfikowany = true;
 
-const wiek = prompt("Ile masz lat?");
-const powitanie = wiek < 10 ? "Hej!" : wiek > 18 ? "Dzień dobry!" : "Hej maluchu :)";
-console.log(powitanie);
+const uzytkownik = (czyZweryfikowany && odp) || "gość";
+console.log(uzytkownik);
