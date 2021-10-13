2b4988043e7d9e50cea848e53fa62cf395865f52
Kristofer <skipperk@interia.pl>
2021-10-14 01:50:14 +0200

    4.1

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const odp = "imię";
-const czyZweryfikowany = true;
+"use strict";
 
-const uzytkownik = (czyZweryfikowany && odp) || "gość";
-console.log(uzytkownik);
+function handleLikePost(step) {
+    let likeCount = 0;
+    return function addLike() {
+        likeCount += step;
+        return likeCount;
+    }
+    console.log("like count:", likeCount);
+}
+
+const like = handleLikePost(1);
+const doubleLike = handleLikePost(2);
+
+console.log(like());
+console.log(like());
+
+console.log(doubleLike());
+console.log(doubleLike());
