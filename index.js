f31bb3a01b1e64723f35e49d8c04bbac51566d44
Kristofer <skipperk@interia.pl>
2021-10-14 02:10:27 +0200

    4.3

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function functionName() {
-    return 'value';
+function konwertujTemperature(celsius, decimalPlaces = 1) {
+    const fahrenheit = celsius * 1.8 + 32;
+    return Number(fahrenheit.toFixed(decimalPlaces));
 }
 
-const user = {
-    name: 'mateusz',
-    joined: '22 Maja, 2002'
-}
-
-const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
-
-function greetUser(name, callback) {
-    return callback(capitalize(name));
-}
-
-const result = greetUser(user.name, name => `Hi there, ${name}`)
-console.log(result);
+console.log(konwertujTemperature(21, 0));
