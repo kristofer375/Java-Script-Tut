a7a7239434dcf9b6456160bd156bec1eb1f42ad0
Kristofer <skipperk@interia.pl>
2021-10-15 20:35:51 +0200

    5.1

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function konwertujTemperature(celsius, decimalPlaces = 1) {
-    const fahrenheit = celsius * 1.8 + 32;
-    return Number(fahrenheit.toFixed(decimalPlaces));
-}
+const num = 42;
+const innyNum = 42;
 
-console.log(konwertujTemperature(21, 0));
+console.log(num === innyNum);
+//primitives - passed by value
+
+const obj = {};
+const innyObj = obj;
+//reference (object) - passed by reference
+innyObj.a = 1;
+console.log("obj", obj);
+console.log("innyObj", innyObj);
