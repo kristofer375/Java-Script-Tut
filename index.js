690c8062a77880c1f1894ae34c769ff7cf8a7c34
Kristofer <skipperk@interia.pl>
2021-10-18 23:23:02 +0200

    8.2

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-navigator.geolocation.getCurrentPosition(position => {
-    console.log(position);
-    getRestaurants(position, restaurants => {
-        console.log(restaurants);
-        console.log('done');
-    })
-});
+//pending - nadal wykonywane
+//fulfilled - gotowe
+//rejected - niepowodzenie wykonania
+
+//const promise = new Promise((resolve, reject) => {
+//    setTimeout(() => resolve('done'), 1000)
+//    setTimeout(() => reject(Error('Promise failed.')), 1000)
+//});
+
+//promise
+//    .then(value => console.log(value))
+//    .catch(error => console.error(error))
+//   .finally(() => console.log('done'))
+
+const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => {
+        resolve(position);
+        }, error => {
+            reject(error);
+        });
+})
+
+promise
+    .then(position => console.log(position))
+    .catch(error => console.error(error))
+    .finally(() => console.log('done'))
