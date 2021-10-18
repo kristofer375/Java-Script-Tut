6b3dd8744ada7b60871c475408a36338669c70d1
Kristofer <skipperk@interia.pl>
2021-10-18 20:39:19 +0200

    6.4

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const menuItems = [
-    { item: "aaa", price: 2.2},
-    { item: "bbb", price: 3.2},
-    { item: "ccc", price: 4.2},
-    { item: "ddd", price: 5.2},
-    { item: "eee", price: 6.2}
-];
+const breakfast = ["nazwa"];
+const dinner = ["inna nazwa", "inna nazwa 2"];
 
-menuItems.reduce((accumulator, menuItem) => {
-    console.log(`
-    accumulator: ${accumulator},
-    menu item price: ${menuItem.price}
-    `)
-    return accumulator + menuItem.price;
-}, 0);
-//console.log(total);
+const wszystko = ["ostatnia nazwa"];
+
+const inne = [
+    ...breakfast,
+    ...dinner,
+    "dodatkowa nazwa",
+    ...wszystko
+];
+console.log(inne);
+
+const index = inne.findIndex(idea => idea === "dodatkowa nazwa");
+
+const ostateczne = [
+    ...inne.slice(0, index),
+    "dodatkowa druga nazwa",
+    ...inne.slice(index + 1)
+]
+
+console.log(ostateczne);
