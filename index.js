d4c5ed991b23b076a618e2d25144ba627aec5aa0
Kristofer <skipperk@interia.pl>
2021-10-21 22:49:49 +0200

    8.4

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-// API: Application Program Interface
-// CRUD - create (POST), read (GET), update (PUT/PATCH), delete (DELETE)
+//fetch('http://jsonplaceholder.typicode.com/posts/1')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
 
-const blogPost = {
-    title: "aaa",
-    body: "bbbbbb",
-    userId: 1
+//const { resolve } = require("path/posix")
+
+//async function getBlogPost() {
+//    const promise = new Promise((resolve, require) => {
+//        setTimeout(() => resolve('blog'), 1000);
+//    })
+//
+//    const result = await promise
+//    console.log(result)
+//    console.log('done')
+//}
+
+async function getPost() {
+    const response = await fetch('http://jsonplaceholder.typicode.com/posts/1')
+    const data = await response.json()
+    console.log(data);
 }
 
-fetch('http://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify(blogPost),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-    .then(response => response.json())
-    .then(data => console.log(data));
+getPost()
